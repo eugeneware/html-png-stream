95d393189704cc2a9cebe680448c233ff4a04ac6
Eugene Ware <eugene@noblesamurai.com>
2015-01-22 21:58:35 +1100

    refactor to use html2png

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var stream = require('stream'),
-    phantomjs = require('phantomjs-bin'),
-    webdriver = require('selenium-webdriver');
+    html2png = require('html2png');
 
 module.exports = HtmlPngStream;
 
@@ -13,49 +12,35 @@ function HtmlPngStream(opts) {
   opts.height = opts.height || 480;
   opts.chromeHeight = opts.chromeHeight || 108;
 
-  var driver = new webdriver.Builder()
-  .withCapabilities({
-     browserName: opts.browser
-  })
-  .build();
-
-  var resized = false;
+  var screenshot = html2png(opts);
 
   var ts = stream.Transform();
   ts._transform = function (chunk, enc, cb) {
     var self = this;
 
     var html = chunk.toString('utf8');
-    if (!resized) {
-      // remove the chrome 'chrome' from the window size
-      driver.manage().window().setSize(opts.width,
-        opts.height + (opts.browser === 'chrome' ? opts.chromeHeight : 0));
-      resized = true;
-    }
-
     if (URL_REGEX.test(html)) {
       // is a URL, get it
-      driver.get(html);
-    } else {
-      driver.executeScript(function (html) {
-          var doc = document.open('text/html');
-          doc.write(html);
-          doc.close();
-        }, html);
-    }
-
-    driver.takeScreenshot().then(function (data) {
-        self.push(new Buffer(data, 'base64'));
+      screenshot.renderUrl(html, function (err, data) {
+        if (err) return cb(err);
+        self.push(data);
         cb();
       });
+    } else {
+      screenshot.render(html, function (err, data) {
+        if (err) return cb(err);
+        self.push(data);
+        cb();
+      });
+    }
   };
 
   ts.once('end', cleanup);
   ts.once('error', cleanup);
 
   function cleanup() {
-    driver && driver.quit();
-    driver = null;
+    screenshot && screenshot.close();
+    screenshot = null;
   }
 
   return ts;
